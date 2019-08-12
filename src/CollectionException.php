e64df84d040c9e7ea3b71d8adf9ab6fb41cd8684
Adrien Martineau <officielghostfire@gmail.com>
2019-08-12 22:13:36 +0200

    Create Collection

---
+++ src/CollectionException.php
@@ -0,0 +1,9 @@
+<?php
+namespace AdrienM\Collection;
+
+class CollectionException extends \Exception
+{
+    const KEY_ALREADY_USE = 500;
+    const KEY_INVALID = 501;
+    const METHOD_DOES_NOT_EXIST = 502;
+}
